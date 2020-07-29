a3f302a42ceb0bced1879f8d882ac92e20cee1e7
Aman Aggarwal <amanaggarwal014@gmail.com>
2020-07-29 20:16:42 +0530

    Getting news ids for top 500 news using volley

--- app/src/main/java/com/amanaggarwal1/newsreader/MainActivity.java
+++ app/src/main/java/com/amanaggarwal1/newsreader/MainActivity.java
@@ -2,10 +2,23 @@ package com.amanaggarwal1.newsreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,8 +34,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         titlesLV = findViewById(R.id.titlesLV);
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,  titles );
 
+        fetchNews();
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,  titles );
         titlesLV.setAdapter(arrayAdapter);
+    }
+
+    private void fetchNews(){
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                            Log.i("LOGCAT", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(getApplicationContext(), "Try again later", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(jsonArrayRequest);
     }
 }
